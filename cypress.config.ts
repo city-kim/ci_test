e659c8c222e1032d6d9ecd5e9a8bbb14b00125b0
ctkim <nkdevil@naver.com>
2023-10-12 18:08:51 +0900

    good

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://localhost:4173/ci_test",
     defaultCommandTimeout: 6000,
   },
-
   component: {
     devServer: {
       framework: "vue",
