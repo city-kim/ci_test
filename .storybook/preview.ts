67000ed02d48fe8b6774ed0c7bf938bf34842e56
ctkim <nkdevil@naver.com>
2023-10-12 11:04:13 +0900

    의심가는부분 업데이트

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,24 @@
+import { setup } from '@storybook/vue3'
+import { createPinia } from 'pinia'
 import type { Preview } from '@storybook/vue3'
+import type { App } from 'vue'
 
 import '../src/assets/scss/main.scss'
 
+const pinia = createPinia()
+
+setup((app: App) => {
+  app.use(pinia)
+})
+
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/
       }
-    }
+    },
   }
 }
 
