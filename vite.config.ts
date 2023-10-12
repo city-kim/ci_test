83e5e358795fd80c861e4b9677378345a1ce0629
ctkim <nkdevil@naver.com>
2023-10-12 20:01:09 +0900

    1

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
+  base: '/ci_test/',
   css: {
     preprocessorOptions: {
       scss: {
